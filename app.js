4304e1f2f62e70913a75fad1ea7d46c6aeb245e0
Fabiha Ahmed <fsafabiha@gmail.com>
2020-11-05 14:41:08 -0500

    added express and env files

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.POST || 4000;
+const port = process.env.PORT || 4000;
 
+//Routes import
 const indexRoute = require("./routes/index.js");
 
 //Routes
