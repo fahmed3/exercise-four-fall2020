5cb57220429501e85b66480d3b2c9d6a3ea3e031
Fabiha Ahmed <fsafabiha@gmail.com>
2020-11-16 23:11:54 -0500

    added individual post queries

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,35 @@
-//Show one blogpost
+//Query one blogpost
 const express = require("express");
-
 const router = express.Router();
 
-router.get("/", (req, res) => res.send("A Blog post"));
+//Require Firebase
+const firebase = require("firebase");
+
+//Initialize Firestore Database
+const db = firebase.firestore();
+
+//Reference a specific collection
+const blogposts = db.collection("blogposts");
+
+router.get("/", (req, res) => res.send("No ID Provided."));
+
+router.get("/:id", (req, res) => {
+  const queryId = req.params.id;
+  blogposts
+    .doc(queryId)
+    .get()
+    .then(function (doc) {
+      if (doc.exists) {
+        // Checking if the document exists
+        const data = doc.data();
+        return res.send(data); //Send data to user who queried
+      } else {
+        return res.send("No document exists.");
+      }
+    })
+    .catch(function (error) {
+      return res.send(error);
+    });
+});
 
 module.exports = router;
