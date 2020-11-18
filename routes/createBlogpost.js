5e23b3a0750994ca4d9624b5fd50366b4df9ffac
Fabiha Ahmed <fsafabiha@gmail.com>
2020-11-17 19:34:14 -0500

    add create post

--- routes/createBlogpost.js
+++ routes/createBlogpost.js
@@ -1,8 +1,43 @@
 //Create a blog post
 const express = require("express");
-
 const router = express.Router();
 
-router.get("/", (req, res) => res.send("Create a blog post"));
+//Require Firebase
+const firebase = require("firebase");
+
+//Initialize Firestore Database
+const db = firebase.firestore();
+
+//Reference a specific collection
+const blogposts = db.collection("blogposts");
+
+const form = `
+    <form action="/create/submit">
+        <input type="text" name="title" placeholder="Title of Post" />
+        <input type="text" name="text" placeholder="Text of Post" />
+        <input type="text" name="author" placeholder="Author" />
+        <button type="submit"> Submit Post </button>
+    </form>`;
+
+// Default route serves form
+router.get("/", (req, res) => res.send(form));
+
+// Route for submitting the form
+router.get("/submit", (req, res) => {
+  const queryParams = req.query; // ?title=words&text=words&author=words
+  // custom IDs for ou posts
+
+  const idFromTitle = queryParams.title.replace(/\s+/g, "-").toLowerCase();
+  blogposts
+    .doc(idFromTitle)
+    .set(queryParams)
+    .then(function (doc) {
+      res.send("Successful Submission");
+    })
+    .catch(function (error) {
+      console.log("error", error);
+      res.send("Failed Submission");
+    });
+});
 
 module.exports = router;
